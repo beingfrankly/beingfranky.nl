26946451735a0ada44685bf28f1534ad5855c478
Frank van Eldijk-Smeding <2766783+beingfrankly@users.noreply.github.com>
2021-10-21 12:54:09 +0200

    chore: ignore generated files

--- build-helpers/generate-sitemap.ts
+++ build-helpers/generate-sitemap.ts
@@ -1,19 +1,18 @@
+const fs = require("fs");
+const path = require('path');
 const { getAllPosts } = require('../utils/mdxUtils');
 
+const SITEMAP_PATH = path.join(process.cwd(), 'public')
 const DOMAIN = 'https://www.beingfrankly.nl';
 const XML_HEADER = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">`;
-
-// const formatDate = date => `${date.toISOString().split('.')[0]}+0:00`;
 
 const xmlUrlWrapper = nodes => `${XML_HEADER}
   ${nodes}
 </urlset>`;
 
 const xmlUrlNode = (post) => {
-  // const url = `${post.slug}${post.slug === '/' ? '' : '/'}`;
-  const loc = `${DOMAIN}/blog/${post.slug}`;
-  // const priority = getPriority(post.slug);
+const loc = `${DOMAIN}/blog/${post.slug}/`;
 
   return `<url>
   <loc>${loc}</loc>
@@ -23,14 +22,19 @@ const xmlUrlNode = (post) => {
 
 const createSitemap = (post) => {
   return {
-    slug: post.slug,
-    lastmod: post.updated_at
+    slug: encodeURI(post.data.slug),
+    lastmod: post.data.updatedOn
   }
 }
 
 const posts = getAllPosts().map(post => createSitemap(post));
-// console.log(getAllPosts());
-// console.log(posts);
-
 const nodes = posts.map(post => xmlUrlNode(post))
-console.log(nodes);
+
+const generateSitemap = () => {
+      fs.writeFile(`${SITEMAP_PATH}/sitemap.xml`, xmlUrlWrapper(nodes), (err) => {
+        if (err) throw err;
+          console.log('The file has been saved!');
+      })
+    }
+
+generateSitemap();
