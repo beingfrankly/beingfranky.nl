5165e6557c2684bcf27a3771b63d827883d8c12f
Frank van Eldijk-Smeding <2766783+beingfrankly@users.noreply.github.com>
2021-11-11 12:43:58 +0100

    fix: add charset in the headers

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,8 @@ function MyApp({ Component, pageProps }) {
       <Head>
           <title key="title">BeingFrankly</title>
           <link rel="canonical" href="https://beingfrankly.nl" key="canonical" />
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="description" content="Making accessibility more accessible for frontend developers." key="description"/>
           <meta property="og:title" content="BeingFrankly" key="og-title" />
           <meta property="og:description" content="Making accessibility more accessible for frontend developers."/>
